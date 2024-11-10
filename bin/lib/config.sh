000211d60e510a07a6392bf29c63b8947b5ee0e2
Pontus Lindman <pontus.v.lindman@gmail.com>
2024-11-10 18:58:18 +0200

    Moved config to projects folder with project-name.yaml naming. Added better support for server names in init,

--- bin/lib/config.sh
+++ bin/lib/config.sh
@@ -1,44 +1,49 @@
 #!/bin/bash
 # config.sh - Working with the config.yaml file
 
+get_config_file_path() {
+    local project_name=$1
+    local project_dir=$(get_projects_root_dir)
+    local config_file="$project_dir/$project_name.yaml"
+    echo $config_file
+}
+
 create_config_file() {
     local project_name=$1
-    local project_dir=$(get_project_dir "$project_name")
     local server_names="${2:-$project_name}"
+    local config_file=$(get_config_file_path "$project_name")
 
     # Path to the template and output configuration file
     TEMPLATE_PATH="$ROOT_DIR/templates/config.yaml.template"
-    OUTPUT_PATH="$project_dir/config.yaml"
-
-    cp "$TEMPLATE_PATH" "$OUTPUT_PATH"
+    cp "$TEMPLATE_PATH" "$config_file"
 
     # Prepare the server names as a list in JSON format for yq
     local server_list
-    IFS=' ' read -r -a server_array <<< "$server_names"
+    if [[ "$server_names" =~ \  ]]; then
+        # If there are spaces, split the server names into an array
+        IFS=' ' read -r -a server_array <<< "$server_names"
+    else
+        # If no spaces, treat it as a single server name
+        server_array=("$server_names")
+    fi
     server_list=$(printf '"%s", ' "${server_array[@]}" | sed 's/, $//')  # Remove trailing comma and space
     server_list="[$server_list]"  # Wrap it in square brackets for the array format
 
     # echo "$server_names converted into $server_list"
 
     # Modify copied template with project details
-    yq -y -i ".name = \"$project_name\"" $OUTPUT_PATH
-    yq -y -i ".servers = $server_list" "$OUTPUT_PATH"
+    yq -y -i ".name = \"$project_name\"" $config_file
+    yq -y -i ".servers = $server_list" $config_file
 
-    print_info "$OUTPUT_PATH created from template $TEMPLATE_PATH for servers $server_names."
-    print_info "You can edit it to change servers and defaults."
-}
-
-get_config_file_name() {
-    local project_name=$1
-    local project_dir=$(get_project_dir "$project_name")
-    local config_file="$project_dir/config.yaml"
-    echo $config_file
+    print_info "Used template $TEMPLATE_PATH."
+    print_info "Created $config_file."
+    print_info "Edit it to add/change/remove servers and change defaults."
 }
 
 # Get server names from project config
-get_server_names() {
+get_server_names_from_config() {
     local project_name=$1
-    local config_file=$(get_config_file_name $project_name)
+    local config_file=$(get_config_file_path $project_name)
 
     yq -r '.servers[]' $config_file | tr '\n' ' '  # Converts newlines to spaces
 }
